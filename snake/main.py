93731ea4d41f87c1f7787f74dee84f625d446d37
AdithiJhanavi <141707677+AdithiJhanavi@users.noreply.github.com>
2023-11-18 10:29:44 +0530

    Add files via upload

---
+++ snake/main.py
@@ -0,0 +1,16 @@
+
+def printBoard():
+    print(f"     ")
+    print(f"")
+    print(f"")
+
+    pass
+    
+
+if __name__ == "__main__":
+    xState = [0, 0, 0, 0, 0, 0, 0, 0]
+    yState = [0, 0, 0, 0, 0, 0, 0, 0]
+    turn = 1 # 1 for X and 0 for O
+    print("Welcome to Tic Tac Toe")
+    print("X's Chance")
+    printBoard()
